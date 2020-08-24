71d0cdd336a23f264efc22e3f991ee6c382f8f9f
brian presta <brian.presta@gmail.comgit commit -m first>
2020-08-23 17:44:18 -0700

    updated foreign key constraints

--- models/index.js
+++ models/index.js
@@ -5,9 +5,12 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category,{foreignKey:'category_id'})
+Product.belongsTo(Category,{foreignKey:'category_id',onDelete:'CASCADE'})
 // Categories have many Products
-Category.hasMany(Product, {foreignKey: 'category_id'})
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
